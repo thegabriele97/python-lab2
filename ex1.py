49eb323bd26b1bc40bb53ca1f3bd702b272b27a6
thegabriele97 <gabriele.lagreca@gmail.com>
2019-03-18 15:06:22 +0100

    ex1 done

--- ex1.py
+++ ex1.py
@@ -1,6 +1,35 @@
 def main():
-    print()
-    show_prompt()
+    lst = []
+
+    print("Welcome to note 3000 revolution!")
+    while True:
+        print()
+        selected_int = int(show_prompt())
+        selected_int -= 1
+
+        if selected_int == 0:
+            note = input("Note: ")
+
+            if note == "":
+                print("Error: insert a name.\n")
+                continue
+            
+            lst.append(note)
+        
+        elif selected_int == 1:
+            note = input("Note: ")
+
+            if not note in lst:
+                print("Error: note isn't present in your list.\n")
+            
+            lst.remove(note)
+        
+        elif selected_int == 2:
+            lst.sort()
+            print(*map(lambda e: "%d. %s" % (lst.index(e) + 1, e), lst), sep=";\n", end=".\n")
+        
+        elif selected_int == 3:
+            break
 
 def show_prompt():
     entries = [
